186a305db29e2cfa101be2b0cf6b9aefcf647a53
nitiankuldeep <nitiankuldeepsingh@gmail.com>
2023-11-18 10:31:29 +0530

    Multipication and Divide

--- calc.c
+++ calc.c
@@ -2,6 +2,12 @@
 int add(int a , int b){
     return a+b;
 }
+int multi(int a , int b){
+    return a*b;
+}
+int divide(int a , int b){
+    return a/b;
+}
 int main(){
     int i,j;
     printf("enter the first number");
@@ -9,5 +15,7 @@ int main(){
      printf("enter the second number");
     scanf("%d",&j);
     printf("%d",add(i,j));
+    printf("%d",multi(i,j));
+    printf("%d",divide(i,j));
     return 0;
 }
